b61457f6aa05af8ded97b3c9747b780ff4ac07e0
Ahoney25 <yshoney25@gmail.com>
2026-01-24 11:39:19 +0530

    Add files via upload

---
+++ DataTypeExample.java
@@ -0,0 +1,13 @@
+class DataTypeExample {
+ public static void main(string[] args) {
+
+     int age = 20;
+     float marks = 85.5f;
+     char grade = 'A';
+     boolean pass = true;
+     system.out.println(age);
+     system.out.println(marks);
+     system.out.println(grade);
+     system.out.println(pass);
+ }
+}
