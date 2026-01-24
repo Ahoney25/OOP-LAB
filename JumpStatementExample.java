215f838de83d17816203c1fc02bab243824a5fd4
Ahoney25 <yshoney25@gmail.com>
2026-01-24 12:33:32 +0530

    Add files via upload

---
+++ JumpStatementExample.java
@@ -0,0 +1,13 @@
+public class JumpStatementExample {
+  public static void main(String[] args) {
+   for (int i = 0; i < 5; i++) {
+     if (i == 3) {
+       continue;
+    } 
+    if (i == 4) {
+       break; 
+     }
+    System.out.println(i);
+   }
+ }
+}
