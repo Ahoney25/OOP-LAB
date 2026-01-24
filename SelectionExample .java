aeb38313977073ac9e1b354e50c5bb65270d3cc0
Ahoney25 <yshoney25@gmail.com>
2026-01-24 11:54:56 +0530

    Add files via upload

---
+++ SelectionExample .java
@@ -0,0 +1,12 @@
+ public class SelectionExample{ 
+     public static void main(String[] args) {
+     int score = 75;
+     
+     if (score >= 50) {
+
+        System.out.println("you passed!");
+  } else {
+        System.out.println("you failed!");
+    }
+  }
+}
