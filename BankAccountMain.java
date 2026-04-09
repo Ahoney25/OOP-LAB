3e50efa5c906724db8e872bb823d7eadb1921824
Ahoney25 <yshoney25@gmail.com>
2026-04-09 14:42:34 +0530

    Add files via upload

---
+++ BankAccountMain.java
@@ -0,0 +1,39 @@
+class BankAccount {
+    private double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        if(balance >= 0) {
+            this.balance = balance;
+        } else {
+            System.out.println("Invalid balance");
+        }
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if(amount <= balance) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient balance");
+        }
+    }
+}
+
+public class BankAccountMain {
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount();
+        acc.setBalance(1000);
+
+        acc.deposit(500);
+        acc.withdraw(300);
+
+        System.out.println("Final Balance: " + acc.getBalance());
+    }
+}
